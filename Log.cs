5323714e92a7c8e1fbba78e2de30ff84f72bff1c
Marcin <marcino451@gmail.com>
2018-06-06 22:22:21 +0200

    added a new class - Log to save all the messages from the app , changed information showed on the screen

---
+++ Log.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+
+namespace ProcessCalc
+{
+    class Log
+    {
+        private StreamWriter log;
+        private static string logDirectory = ConfigurationManager.AppSettings["LogDirectory"];
+        private static string logFile = ConfigurationManager.AppSettings["LogFile"];
+
+        // method for creating a directory for a log if it doesn't exist
+        private static void CreateDirectoryForLog()
+        {
+            if (!Directory.Exists(@logDirectory))
+            {
+                Directory.CreateDirectory(@logDirectory);
+            }
+        }
+
+        // write errors to log
+        public void ToLogAfterError(string error)
+        {
+            Log.CreateDirectoryForLog();
+            using (log = new StreamWriter(@logDirectory + logFile, true))
+            {
+                log.WriteLine(DateTime.Now + ": Error: " + error);
+            }
+        }
+
+        // write to log info about killed processes
+        public void ToLogKilledProcessInfo(string id, string ProcessName, string owner)
+        {
+            Log.CreateDirectoryForLog();
+            using (log = new StreamWriter(@logDirectory + logFile, true))
+            {
+                log.WriteLine(DateTime.Now + " : " + "Killed process " + ProcessName + " with ID " + id + "and owner " + owner);
+            }
+        }
+
+        // write to log messages from the app.
+        public void ToLogWriteSth (string message)
+        {
+            Log.CreateDirectoryForLog();
+            using (log = new StreamWriter(@logDirectory + logFile, true))
+            {
+                log.WriteLine(DateTime.Now + " : " + message);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,21 +1,27 @@
 ﻿/**
- * calculating a number of processess ran by user and showing this number
- * checking if it exceeds the limits
+ * Process Calculator
+ * Calculates a number of processess ran by user, shows this number
+ * abd checks if it exceeds the limits written in the App.config
+ * ERROR CODES :
+ * 0 - No error (limit is not excedeed)
+ * 1 -  too much processes
+ * 2 -  no user given :)
 **/
 
 using System;
-using System.Configuration;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
 using System.Linq;
 using System.Management;
 using System.Runtime.InteropServices;
-using System.Security.Principal;
+
 namespace ProcessCalc
 {
     class Program
     {
         private static string processToCalculate = ConfigurationManager.AppSettings["ProcessToCalculate"];
+        
 
         [DllImport("advapi32.dll", SetLastError = true)]
         private static extern bool OpenProcessToken(IntPtr ProcessHandle, uint DesiredAccess, out IntPtr TokenHandle);
@@ -23,30 +29,6 @@ namespace ProcessCalc
         [DllImport("kernel32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         private static extern bool CloseHandle(IntPtr hObject);
-        
-        // a method for getting a process owner by process
-        public static string GetProcessUser(Process process)
-        {
-            IntPtr processHandle = IntPtr.Zero;
-            try
-            {
-                OpenProcessToken(process.Handle, 8, out processHandle);
-                WindowsIdentity wi = new WindowsIdentity(processHandle);
-                string user = wi.Name;
-                return user.Contains(@"\") ? user.Substring(user.IndexOf(@"\") + 1) : user;
-            }
-            catch
-            {
-                return null;
-            }
-            finally
-            {
-                if (processHandle != IntPtr.Zero)
-                {
-                    CloseHandle(processHandle);
-                }
-            }
-        }
 
         // a method for getting a process owner by processId
         private static string GetProcessUser(string processId)
@@ -66,33 +48,58 @@ namespace ProcessCalc
 
         static void Main(string[] args) 
         {
+            Log log = new Log(); // object of the Log class
+
+            if (args.Length == 0)
+            {
+                log.ToLogAfterError("No user name.");
+                Console.WriteLine("Error : No user name.");
+                Environment.Exit(2);
+            }
+
             String username = args[0]; // user name got from the parameter
 
             Console.WriteLine("Checking user : "  + username);
+            log.ToLogWriteSth("Checking user : " + username);
             Process[] pname = Process.GetProcessesByName(processToCalculate);
+            IEnumerable<Process> pnam = pname.OrderBy(pnames => pnames.StartTime);
+            
             int procNumb = 0; // number of processes
+            Console.WriteLine("\nAll processess of {0} :", processToCalculate);
             for (int i = 0; i < pname.Length; i++)
             {
-                string id = pname[i].Id + ""; // getting an Id from the process in the table
+                string id = pname[i].Id + ""; // getting an Id from the process in the table pname
                 string own = GetProcessUser(id); // getting an owner of running process
-                
-                Console.WriteLine(pname[i].Id + " " + own);
+                Console.WriteLine(pname[i].Id + " " + processToCalculate + " " + own);
                 if (own.Equals(username))
                 {
                     procNumb++;
                 }
             }
 
-            Console.WriteLine("User " + username +" have ran " + procNumb + " processes");
+            Console.WriteLine("\nUser " + username +" have ran " + procNumb + " processes");
             int number = Int32.Parse(ConfigurationManager.AppSettings[username]); // a number from the config file
-            if (procNumb > number) 
+            if (procNumb > number) // checking a number of processess and deciding about errorcode
             {
-                Console.WriteLine("You have ran too many processess.");
+                Console.WriteLine("\nUser have ran too many processess.");
+                log.ToLogAfterError("User have ran too many processess.");
                 Console.WriteLine("Program ended with errorcode 1.");
-                Environment.Exit(1);
+                //foreach (Process p in pnam)
+                //{
+                //    int i = pnam.Count();
+                //    int limit = procNumb - number;
+                //    if (i <= limit)
+                //    {
+                //        string id = (pnam.ElementAt(i).Id).ToString();
+                //        string owner = GetProcessUser(id);
+                //        string ProcessName = pnam.ElementAt(i).ProcessName;
+                //        log.ToLogKilledProcessInfo(id, ProcessName, owner);
+                //    }
+                //}
             }
             else
             {
+                log.ToLogAfterError("Program ended with errorcode 0.");
                 Console.WriteLine("Program ended with errorcode 0.");
                 Environment.Exit(0);
             }
